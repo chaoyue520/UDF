c6dc0443f11512a7e3b4ef7982b374aae15a4d65
chaoyue520 <32260272+chaoyue520@users.noreply.github.com>
2017-10-19 14:52:32 +0800

    udf

---
+++ FindSpecialFieldWords.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys
+
+for line in sys.stdin:
+    order_sn, delivery_address = line.strip().split("\t")
+
+    found = "N"
+    pos = delivery_address.decode("utf8").find(u'\u5927\u5b66')
+    if(pos > -1):
+        found = "Y"
+    print "\t".join([order_sn, delivery_address, found])
+
+
+
+#使用 Python 实现的 UDF 完成后，需要通过 ADD FILE 指令添加至 Hive 中进行注册，无需起别名：
+hive> ADD FILE ./FindSpecialFieldWords.py
+
+
+
+
